9665feb0a27bd583fc331972ea26b0da4fbd0486
Simon Russell <simon@playup.com>
2011-11-02 15:27:21 +1100

    Move spec_helper to the correct position.

--- spec/dew/models/fog_model_spec.rb
+++ spec/dew/models/fog_model_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
+require 'spec_helper'
 
 describe FogModel do
   let (:fog_object) { double('Fog Object', :id => 'i-12345') }
